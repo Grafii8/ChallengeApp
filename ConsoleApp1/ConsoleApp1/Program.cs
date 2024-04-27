ffb75819856dd58c8c3207a4ea01bf05026cf792
Grafii <mweronikaa@gmail.com>
2024-04-27 18:01:43 +0100

    program cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" World!");
+﻿using System.ComponentModel.Design;
+
+var firstname = "Ewa";
+var gender = "kobieta";
+int age = 30;
+if (gender == "kobieta" && age == 30)
+{
+    Console.WriteLine("kobieta lat 30");
+}
+else if (firstname == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (age < 18 && gender == "mezczyzna")
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
+
